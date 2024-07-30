4a5f362a46544929e56fe93bed88942560c49786
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-07-29 23:45:24 -0300

    feat: initial load data on dashboard + search

--- src/pages/Dashboard/components/Searchbar/index.tsx
+++ src/pages/Dashboard/components/Searchbar/index.tsx
@@ -1,22 +1,37 @@
-import { HiRefresh } from "react-icons/hi";
-import { useHistory } from "react-router-dom";
-import Button from "~/components/Buttons";
-import { IconButton } from "~/components/Buttons/IconButton";
-import TextField from "~/components/TextField";
-import routes from "~/router/routes";
-import * as S from "./styles";
-export const SearchBar = () => {
+import { HiRefresh } from 'react-icons/hi';
+import { useHistory } from 'react-router-dom';
+import Button from '~/components/Buttons';
+import { IconButton } from '~/components/Buttons/IconButton';
+import TextField from '~/components/TextField';
+import routes from '~/router/routes';
+import * as S from './styles';
+import { useState } from 'react';
+export const SearchBar = ({ onSearch }: any) => {
+  const [searchValue, setSearchValue] = useState('');
   const history = useHistory();
+
+  const handleSearchChange = (e: any) => {
+    setSearchValue(e.target.value.toLowerCase());
+  };
+
+  const handleSearchKeyUp = () => {
+    onSearch(searchValue);
+  };
 
   const goToNewAdmissionPage = () => {
     history.push(routes.newUser);
   };
-  
+
   return (
     <S.Container>
-      <TextField  placeholder="Digite um CPF válido" />
+      <TextField
+        placeholder='Digite um CPF válido'
+        value={searchValue}
+        onChange={handleSearchChange}
+        onKeyUp={handleSearchKeyUp}
+      />
       <S.Actions>
-        <IconButton aria-label="refetch">
+        <IconButton aria-label='refetch'>
           <HiRefresh />
         </IconButton>
         <Button onClick={() => goToNewAdmissionPage()}>Nova Admissão</Button>
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,41 @@
-import Collumns from "./components/Columns";
-import * as S from "./styles";
-import { SearchBar } from "./components/Searchbar";
+import { useEffect, useState } from 'react';
+
+import * as RegistrationService from '~/services/registrations';
+import { Registration } from '~/types';
+
+import Collumns from './components/Columns';
+import { SearchBar } from './components/Searchbar';
+import * as S from './styles';
 
 const DashboardPage = () => {
+  const [registrations, setRegistrations] = useState<Registration[]>([]);
+  const [filteredRegistrations, setFilteredRegistrations] =
+    useState<Registration[]>(registrations);
+
+  useEffect(() => {
+    const fetchRegistrations = async () => {
+      const data = await RegistrationService.get();
+      setRegistrations(data);
+      setFilteredRegistrations(data);
+    };
+    fetchRegistrations();
+  }, []);
+
+  const handleSearch = (searchValue: string) => {
+    if (searchValue) {
+      const filtered = registrations.filter((registration) =>
+        registration.cpf.includes(searchValue)
+      );
+      setFilteredRegistrations(filtered);
+    } else {
+      setFilteredRegistrations(registrations);
+    }
+  };
+
   return (
     <S.Container>
-      <SearchBar />
-      <Collumns registrations={[]} />
+      <SearchBar onSearch={handleSearch} />
+      <Collumns registrations={filteredRegistrations ?? registrations} />
     </S.Container>
   );
 };
