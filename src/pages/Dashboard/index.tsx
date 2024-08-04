7647f8ab36a61fa0ba779a0e4a502e78927adae2
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-08-03 21:54:03 -0300

    refactor: update dashboad page component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,41 +1,87 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useHistory } from 'react-router-dom';
+import { HiRefresh } from 'react-icons/hi';
 
+import routes from '~/router/routes';
 import * as RegistrationService from '~/services/registrations';
-import { Registration } from '~/types';
+import { removeSpecialChars } from '~/utils/formatters';
+import { isValidCPF } from '~/utils/validations';
+import { useLoader } from '~/hooks/loader';
 
-import Collumns from './components/Columns';
-import { SearchBar } from './components/Searchbar';
+import { Registration } from '~/types';
+import { Collumns, SearchBar } from './components';
+import { Button, IconButton } from '~/components/Buttons';
+
 import * as S from './styles';
 
 const DashboardPage = () => {
   const [registrations, setRegistrations] = useState<Registration[]>([]);
   const [filteredRegistrations, setFilteredRegistrations] =
     useState<Registration[]>(registrations);
+  const { setLoading } = useLoader();
+  const searchBarRef = useRef<{ clearSearch: () => void }>(null);
+  const history = useHistory();
 
   useEffect(() => {
-    const fetchRegistrations = async () => {
-      const data = await RegistrationService.get();
-      setRegistrations(data);
-      setFilteredRegistrations(data);
-    };
     fetchRegistrations();
   }, []);
 
-  const handleSearch = (searchValue: string) => {
-    if (searchValue) {
-      const filtered = registrations.filter((registration) =>
-        registration.cpf.includes(searchValue)
-      );
-      setFilteredRegistrations(filtered);
+  const fetchRegistrations = async () => {
+    setLoading(true);
+    const data = await RegistrationService.get();
+    setRegistrations(data);
+    setFilteredRegistrations(data);
+
+    if (searchBarRef.current) {
+      searchBarRef.current.clearSearch();
+    }
+
+    setLoading(false);
+  };
+
+  const handleSearch = async (searchValue: string) => {
+    if (isValidCPF(searchValue)) {
+      setLoading(true);
+      const cpf = removeSpecialChars(searchValue);
+      const data = await RegistrationService.getByCPF(cpf);
+      setFilteredRegistrations(data);
     } else {
       setFilteredRegistrations(registrations);
     }
+    setLoading(false);
+  };
+
+  const goToNewRegistrationPage = () => {
+    history.push(routes.newRegistration);
   };
 
   return (
     <S.Container>
-      <SearchBar onSearch={handleSearch} />
-      <Collumns registrations={filteredRegistrations ?? registrations} />
+      <S.Menu>
+        <SearchBar ref={searchBarRef} onSearch={handleSearch} />
+        <S.Actions>
+          <IconButton
+            $hasBorder={true}
+            color='#64a98c'
+            aria-label='refetch'
+            onClick={fetchRegistrations}
+          >
+            <HiRefresh />
+          </IconButton>
+          <Button
+            color='#FFFFFF'
+            $size='large'
+            $bgColor='#64a98c'
+            onClick={() => goToNewRegistrationPage()}
+          >
+            Nova Admissão
+          </Button>
+        </S.Actions>
+      </S.Menu>
+      <Collumns
+        registrations={filteredRegistrations}
+        onUpdate={fetchRegistrations}
+      />
     </S.Container>
   );
 };
---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  padding: 24px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+`;
+
+export const Actions = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  gap: 16px;
+`;
