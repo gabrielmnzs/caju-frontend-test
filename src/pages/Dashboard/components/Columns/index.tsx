6a15b3376eea434d6b9f50e259a064e324342a88
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-08-03 21:50:06 -0300

    refactor: update columns component

--- src/pages/Dashboard/components/Columns/index.tsx
+++ src/pages/Dashboard/components/Columns/index.tsx
@@ -1,46 +1,47 @@
 import { Registration } from '~/types';
+import { Status } from '~/constants';
 
 import RegistrationCard from '../RegistrationCard';
 import * as S from './styles';
 
 const allColumns = [
-  { status: 'REVIEW', title: 'Pronto para revisar' },
-  { status: 'APPROVED', title: 'Aprovado' },
-  { status: 'REPROVED', title: 'Reprovado' },
+  { status: Status.REVIEW, title: 'Pronto para revisar' },
+  { status: Status.APPROVED, title: 'Aprovado' },
+  { status: Status.REPROVED, title: 'Reprovado' },
 ];
 
-type Props = {
+interface ColumnsProps {
   registrations?: Registration[];
-};
-const Collumns = (props: Props) => {
+  onUpdate: () => void;
+}
+
+export const Collumns = ({ registrations, onUpdate }: ColumnsProps) => {
   return (
     <S.Container>
       {allColumns.map((collum) => {
         return (
-          <S.Column status={collum.status} key={collum.title}>
-            <>
-              <S.TitleColumn status={collum.status}>
-                {collum.title}
-              </S.TitleColumn>
-              <S.CollumContent>
-                {props?.registrations
-                  ?.filter(
-                    (registration) => registration.status === collum.status
-                  )
-                  .map((registration) => {
-                    return (
-                      <RegistrationCard
-                        data={registration}
-                        key={registration.id}
-                      />
-                    );
-                  })}
-              </S.CollumContent>
-            </>
+          <S.Column $status={collum.status} key={collum.title}>
+            <S.TitleColumn $status={collum.status}>
+              {collum.title}
+            </S.TitleColumn>
+            <S.CollumContent>
+              {registrations
+                ?.filter(
+                  (registration) => registration.status === collum.status
+                )
+                .map((registration) => {
+                  return (
+                    <RegistrationCard
+                      data={registration}
+                      key={registration.id}
+                      onUpdate={onUpdate}
+                    />
+                  );
+                })}
+            </S.CollumContent>
           </S.Column>
         );
       })}
     </S.Container>
   );
 };
-export default Collumns;
