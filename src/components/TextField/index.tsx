7b2b44619aafabd6b734416e73180fbad4f81efe
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-08-03 21:41:54 -0300

    refactor: adjusments in text field component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,38 +1,31 @@
-import React, { InputHTMLAttributes } from "react";
-import styled from "styled-components";
+import { InputHTMLAttributes, forwardRef } from 'react';
 
-export const Input = styled.input`
-  padding: 0 8px;
-  vertical-align: middle;
-  border-radius: 2px;
-  width: 100%;
-  min-height: 36px;
-  background-color: #ffffff;
-  border: 1px solid rgba(36, 28, 21, 0.3);
-  transition: all 0.2s ease-in-out 0s;
-  font-size: 16px;
-  line-height: 18px;
-  font-weight: normal;
-  border-radius:8px;
-  :focus {
-    outline: none;
-    border: 1px solid #007c89;
-    box-shadow: inset 0 0 0 1px #007c89;
-  }
-`;
-type Props = {
+import * as S from './styles';
+
+type TextFieldProps = {
   label?: string;
   error?: string;
-} & InputHTMLAttributes<any>;
+} & InputHTMLAttributes<HTMLInputElement>;
 
-const TextField = (props: Props) => {
+const TextField: React.ForwardRefRenderFunction<
+  HTMLInputElement,
+  TextFieldProps
+> = ({ label, error, ...props }: TextFieldProps, ref) => {
   return (
-    <div>
-      <label htmlFor={props.id}>{props.label}</label>
-      <Input {...props} />
-      <span style={{fontSize: 12, color: 'red'}}>{props.error}</span>
-    </div>
+    <S.Container>
+      {label && (
+        <label htmlFor={props.id} style={{ marginBottom: '8px' }}>
+          {label}
+        </label>
+      )}
+      <S.Input ref={ref} {...props} />
+      {error && (
+        <span style={{ fontSize: 12, color: 'red', height: '15px' }}>
+          {error}
+        </span>
+      )}
+    </S.Container>
   );
 };
 
-export default TextField;
+export default forwardRef(TextField);
