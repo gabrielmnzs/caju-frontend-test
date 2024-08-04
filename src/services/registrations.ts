2e971030feb413b8ba30a8dd7bcd0468fb561805
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-08-03 21:48:13 -0300

    feat: services functions get, post, put, delete

--- src/services/registrations.ts
+++ src/services/registrations.ts
@@ -1,9 +1,34 @@
-import api from '../utils/axios';
+import api from '../utils/network/api';
 
 import { Registration } from '~/types';
 
 export const get = async () => {
   const response = await api.get<Registration[]>('/registrations');
+
+  return response.data;
+};
+
+export const getByCPF = async (cpf: string) => {
+  const response = await api.get<Registration[]>(`/registrations?cpf=${cpf}`);
+
+  return response.data;
+};
+
+export const create = async (payload: Omit<Registration, 'id'>) => {
+  console.log(payload);
+  const response = await api.post('/registrations', payload);
+
+  return response.data;
+};
+
+export const put = async (payload: Registration) => {
+  const response = await api.put(`/registrations/${payload.id}`, payload);
+
+  return response.data;
+};
+
+export const remove = async (payload: Registration) => {
+  const response = await api.delete(`/registrations/${payload.id}`);
 
   return response.data;
 };
