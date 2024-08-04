034453ea7b382286fcbdade730003bd4e2bb61d1
Gabriel Menezes <gabrielmnzs7@gmail.com>
2024-08-03 21:40:41 -0300

    chore: update config to jest run tests

--- jest.config.js
+++ jest.config.js
@@ -2,13 +2,16 @@
 export default {
   verbose: true,
   preset: "ts-jest",
+  setupFilesAfterEnv: ["<rootDir>/src/setup-test.ts"],
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", { isolatedModules: true }],
+    "^.+\\.(ts|tsx)?$": ["ts-jest", { isolatedModules: true }],
     "^.+\\.(js|jsx)$": "babel-jest",
   },
   moduleNameMapper: {
     "^~/(.+)": "<rootDir>/src/$1",
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
   },
-//   setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
 };
